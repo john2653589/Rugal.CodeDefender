dbf4e3e5b0f6d1a68239de00ffb30966104aba22
Rugal <john2653589@gmail.com>
2023-09-01 18:54:01 +0800

    v1.0.0.3-beta

--- Rugal.CodeDefender/Extention/IFormFileExtention.cs
+++ Rugal.CodeDefender/Extention/IFormFileExtention.cs
@@ -6,6 +6,17 @@ namespace Rugal.CodeDefender.Extention
     public static class IFormFileExtention
     {
         public const string NotAllowMessage = "不允許的檔案類型";
+        public static void VerifyImage(this IFormFile File)
+        {
+            var Types = new[] { FileType.Jpg, FileType.Jpeg, FileType.Png };
+            if (!IsVerify(File, Types))
+                throw new Exception(NotAllowMessage);
+        }
+        public static void VerifyPdf(this IFormFile File)
+        {
+            if (!IsVerify(File, FileType.Pdf))
+                throw new Exception(NotAllowMessage);
+        }
         public static void Verify(this IFormFile File, params FileType[] Types)
         {
             if (!IsVerify(File, Types))
